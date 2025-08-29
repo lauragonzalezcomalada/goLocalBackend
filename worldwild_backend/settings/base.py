2f22488abc102dd9091e6e2baae5405548450589
lauragonzalezcomalada <lauragonzalezcomalada@gmail.com>
2025-08-29 13:31:40 -0300

    add whitenoisemiddleware

--- worldwild_backend/settings/base.py
+++ worldwild_backend/settings/base.py
@@ -62,7 +62,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware'
+    'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 CORS_ALLOW_ALL_ORIGINS = True
 
